9c6e861fbc30f5efde51e166275228b84904ad54
theppsh <734331254@qq.com>
2018-08-28 09:16:25 +0800

    add config and main.py

---
+++ config/config.py
@@ -0,0 +1,43 @@
+# coding=UTF-8
+
+class CFG():
+    voc_dir='/home/theppsh/workspace/data/voc/VOCdevkit/VOC2007'
+
+    device_id=0
+    use_cuda=True
+
+    weights_dir='./weights/'
+
+    caffe_model="./models/vgg16_caffe_pretrain.pth"
+    use_caffe=True
+    use_caffe_classifier=True
+    
+    # loc_mean=[.0,.0,.0,.0]
+    # loc_std=[1.,1.,2.,2.]
+
+    intput_w=300
+    input_h=300
+
+    rand_seed=0
+    
+    epochs=10
+    lr=1e-3
+    weight_decay=0.0005
+    use_adam=False
+
+    out_thruth_thresh=.5
+    pos_thresh=.5
+
+    def _print(self):
+        print('Config')
+        print('{')
+        for k in self._attr_list():
+            print('%s=%s'% (k,getattr(self,k)) )
+        print("}")
+    @staticmethod
+    def _attr_list():
+        return [k for k in CFG.__dict__.keys() if not k.startswith('_') ] 
+            
+    # rpn path
+   
+cfg=CFG()
---
+++ main.py
@@ -0,0 +1,10 @@
+# coding=UTF-8
+# author=theppsh
+
+from config import cfg
+
+def main():
+    cfg._print()
+
+if __name__ == '__main__':
+    main()
