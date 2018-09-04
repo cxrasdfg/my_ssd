d77267763ca36b52c4cf3a22cceacdb7352d7455
theppsh <734331254@qq.com>
2018-09-04 20:06:27 +0800

    add tqdm ifo

--- net/ssd/loss.py
+++ net/ssd/loss.py
@@ -29,18 +29,20 @@ class SSDLoss(torch.nn.Module):
 
         cls_loss=-pos_cls[torch.arange(num_pos).long(),(pos_label).long()].log().sum()\
             -(neg_cls[:,0].log().sum() if len(neg_cls)!=0 else 0)
-        
+
+        tqdm.write('cls_loss: %.5f'%(cls_loss.item()),end=' ||')
+
         ttt=pos_cls[torch.arange(num_pos ).long(),(pos_label).long()].max()
         _,acc=pos_cls[:,:].max(dim=1)
         acc=acc
         acc=acc.long()
         acc=(acc==pos_label).sum().float()/num_pos
-        tqdm.write("SSD class loss : max prob=%.5f, acc=%.5f" \
-            %(ttt,acc),end=",\t ")
+        tqdm.write("class branch: max prob=%.5f, acc=%.5f" \
+            %(ttt,acc),end=" || ")
 
         # smooth l1...
         reg_loss=_smooth_l1_loss(out_box,gt_box,cfg.sigma)
-
+        tqdm.write('reg_loss: %.5f'%(reg_loss.item()) ,end=' || ')
         # loss=cls_loss/n_cls+reg_loss/num_pos*_lambda
         loss=cls_loss+reg_loss*_alpha
         loss/=num_pos
